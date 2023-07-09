e30f230a3b5221ffebe089808c06fedc87224ec6
Joshua Maitland-Soto <maitland.joshua@gmail.com>
2023-07-09 01:01:34 -0400

    Created basic navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root')
 
