7256bd00b30a8491b52f7be3032cb15fec9ef77b
Josh Maitland <josh@icebergcyber.com>
2026-09-16 22:20:15 -0400

    chore: add Vitest test runner

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") }
-  }
+  },
+  test: {
+    environment: 'jsdom',
+  },
 })
